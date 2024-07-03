8d86b83e23bd361dcd375834729268855df26bbc
Amal221200 <amalmurikkoli2000@gmail.com>
2024-07-03 11:34:32 +0530

    hovering border button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
                 {children}
               </div>
             </main>
-            <MobileSidebarButton />
+              <MobileSidebarButton />
           </ThemeProvider>
         </MobileSidebarProvider>
       </body>
--- src/components/buttons/MobileSidebarButton.tsx
+++ src/components/buttons/MobileSidebarButton.tsx
@@ -2,16 +2,42 @@
 import React, { use } from 'react'
 import { MobileSidebarContext, TMobileSidebarContext } from '../providers/MobileSidebarProvider'
 import { Menu, X } from 'lucide-react'
+import { HoverBorderGradient } from '../ui/HoverBorderGradient';
 
 const MobileSidebarButton = () => {
     const { open, toggleMobileSidebar } = use(MobileSidebarContext) as TMobileSidebarContext
     return (
-        <button onClick={toggleMobileSidebar} className="fixed bottom-5 left-5 z-[100] block rounded-md border border-white-100/[0.2] bg-black-100 p-2 text-sm text-white-200 transition duration-200 sm:hidden">
+        <HoverBorderGradient
+            containerClassName="fixed bottom-5 left-5 z-[100] rounded-md sm:hidden"
+            as="button"
+            className="flex items-center bg-white text-black dark:bg-black dark:text-white"
+            onClick={toggleMobileSidebar}
+        >
             {
                 open ? <X /> : <Menu />
             }
-        </button>
+        </HoverBorderGradient>
     )
 }
+const AceternityLogo = () => {
+    return (
+        <svg
+            width="66"
+            height="65"
+            viewBox="0 0 66 65"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3 w-3 text-black dark:text-white"
+        >
+            <path
+                d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+                stroke="currentColor"
+                strokeWidth="15"
+                strokeMiterlimit="3.86874"
+                strokeLinecap="round"
+            />
+        </svg>
+    );
+};
 
 export default MobileSidebarButton
